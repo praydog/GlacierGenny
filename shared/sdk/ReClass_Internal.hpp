8fe76743954998e30e24232a3dd0418ce57d82e4
praydog <praydog@praydog.com>
2021-02-17 20:05:12 -0800

    ReClass

--- shared/sdk/ReClass_Internal.hpp
+++ shared/sdk/ReClass_Internal.hpp
@@ -165,9 +165,9 @@ public:
 
 class TEntityRef_RECLASS {
 public:
-    class ZCameraEntity_RECLASS* base; // 0x0000
-    void* ptr;                         // 0x0008
-};                                     // Size: 0x0010
+    class ZEntityImpl_Inner* base; // 0x0000
+    class N00000778* ptr;          // 0x0008
+};                                 // Size: 0x0010
 
 class N000000F7 {
 public:
@@ -211,10 +211,12 @@ public:
 
 class ZActorManager {
 public:
-    char pad_0000[8];                   // 0x0000
-    class TEntityRef_RECLASS N00000B49; // 0x0008
-    char pad_0018[1072];                // 0x0018
-};                                      // Size: 0x0448
+    char pad_0000[8];                        // 0x0000
+    class TEntityRef_RECLASS actors[500];    // 0x0008
+    char pad_1F48[32];                       // 0x1F48
+    class TEntityRef_RECLASS N000017AF[500]; // 0x1F68
+    char pad_3EA8[216];                      // 0x3EA8
+};                                           // Size: 0x3F80
 
 class SaveSystem {
 public:
@@ -263,9 +265,10 @@ public:
 
 class N00000F7F {
 public:
-    int32_t N00000F80; // 0x0000
-    char pad_0004[20]; // 0x0004
-};                     // Size: 0x0018
+    int32_t N00000F80;            // 0x0000
+    char pad_0004[12];            // 0x0004
+    class ZEntityImpl_Inner* ent; // 0x0010
+};                                // Size: 0x0018
 
 class N00000FEC {
 public:
@@ -291,18 +294,23 @@ public:
 
 class ZSimpleModuleBase {
 public:
-    char pad_0000[16];                  // 0x0000
-    char* name;                         // 0x0010
-    char pad_0018[72];                  // 0x0018
-    class ModuleInterfaces* interfaces; // 0x0060
-};                                      // Size: 0x0068
+    char pad_0000[8];                                // 0x0000
+    uint16_t id_maybe;                               // 0x0008
+    char pad_000A[6];                                // 0x000A
+    char* name;                                      // 0x0010
+    class ModuleInterfaces2* interfaces_alternative; // 0x0018
+    char pad_0020[64];                               // 0x0020
+    class ModuleInterfaces* interfaces;              // 0x0060
+    uint16_t index;                                  // 0x0068
+    char pad_006A[6];                                // 0x006A
+};                                                   // Size: 0x0070
 
 class ModuleInterfaces {
 public:
-    class IComponentInterface_RECLASS* (*systems)[64]; // 0x0000
-    void* systems_last;                                // 0x0008
-    void* systems_allocated_last;                      // 0x0010
-};                                                     // Size: 0x0018
+    class IComponentInterface_RECLASS* (*systems)[100]; // 0x0000
+    void* systems_last;                                 // 0x0008
+    void* systems_allocated_last;                       // 0x0010
+};                                                      // Size: 0x0018
 
 class IComponentInterface_RECLASS {
 public:
@@ -310,6 +318,188 @@ public:
 };                    // Size: 0x0008
 
 class ZApplicationEngineWin32 {
+public:
+    char pad_0000[8]; // 0x0000
+};                    // Size: 0x0008
+
+class ModuleInterfaces2 {
+public:
+    char pad_0000[48];                 // 0x0000
+    class ModuleInterfaces interfaces; // 0x0030
+    char pad_0048[8];                  // 0x0048
+};                                     // Size: 0x0050
+
+class GameEntityList {
+public:
+    class ZEntityImpl_Inner* (*start)[256]; // 0x0000
+    void* last;                             // 0x0008
+    void* allocated_end;                    // 0x0010
+};                                          // Size: 0x0018
+
+class N00001319 {
+public:
+    void* unk;                      // 0x0000
+    class GameEntityList N0000131B; // 0x0008
+    char pad_0020[24];              // 0x0020
+};                                  // Size: 0x0038
+
+class ZGameEntityManager {
+public:
+    char pad_0000[16];                // 0x0000
+    class N00001319 entity_lists[27]; // 0x0010 0 = doors, 1 = items, 2 = volume triggers 3 = math stuff...?, 8 = math
+                                      // distance stuff, 16 = evacuation nodes, 24 = bone attach
+    char pad_05F8[40];                // 0x05F8
+};                                    // Size: 0x0620
+
+class ZEntityImpl_Inner {
+public:
+    class EntityData* entity_data; // 0x0000
+    int32_t N000011FA;             // 0x0008
+    int32_t N00000732;             // 0x000C
+};                                 // Size: 0x0010
+
+class EntityData {
+public:
+    char pad_0000[8];                       // 0x0000
+    class EntityDataFields* fields1;        // 0x0008
+    class EntityDataFields* fields2;        // 0x0010
+    char pad_0018[8];                       // 0x0018
+    class EntityDataComponents* components; // 0x0020
+    char pad_0028[16];                      // 0x0028
+    class EntityDataFunctions* functions;   // 0x0038
+    class EntityDataFunctions* functions2;  // 0x0040
+    int32_t next_component_offset;          // 0x0048
+    char pad_004C[4];                       // 0x004C
+    int32_t last_component_offset;          // 0x0050
+    char pad_0054[12];                      // 0x0054
+    class N000007A5* N000015EF;             // 0x0060
+    char pad_0068[8];                       // 0x0068
+    int32_t N0000146A;                      // 0x0070
+    char pad_0074[260];                     // 0x0074
+};                                          // Size: 0x0178
+
+class EntityDataFields {
+public:
+    class N00001372 (*start)[64]; // 0x0000
+    void* last;                   // 0x0008
+    void* allocated_last;         // 0x0010
+};                                // Size: 0x0018
+
+class N000012D3 {
+public:
+    char pad_0000[8];               // 0x0000
+    class GameEntityList N00001238; // 0x0008
+    char pad_0020[32];              // 0x0020
+    class GameEntityList N0000114E; // 0x0040
+    char pad_0058[32];              // 0x0058
+    class GameEntityList N00001153; // 0x0078
+    char pad_0090[32];              // 0x0090
+    class GameEntityList N00001159; // 0x00B0
+};                                  // Size: 0x00C8
+
+class N00001372 {
+public:
+    char pad_0000[40]; // 0x0000
+};                     // Size: 0x0028
+
+class EntityDataComponents {
+public:
+    class HeirarchyDescriptor (*start)[64]; // 0x0000
+    void* last;                             // 0x0008
+    void* allocated_last;                   // 0x0010
+};                                          // Size: 0x0018
+
+class N0000143D {
+public:
+    class Type_CLASS* klass; // 0x0000
+    char pad_0008[8];        // 0x0008
+};                           // Size: 0x0010
+
+class N00001753 {
+public:
+    char pad_0000[16]; // 0x0000
+};                     // Size: 0x0010
+
+class N00001755 {
+public:
+    char pad_0000[16]; // 0x0000
+};                     // Size: 0x0010
+
+class ZActorProxyEntity_RECLASS {
+public:
+    char pad_0000[8];                  // 0x0000
+    class ZEntityImpl_Inner N000015CE; // 0x0008
+    char pad_0018[8];                  // 0x0018
+    class N00001753 m_rActor;          // 0x0020
+    class N00001755 m_AccidentContext; // 0x0030
+    char pad_0040[72];                 // 0x0040
+};                                     // Size: 0x0088
+
+class EntityDataFunctions {
+public:
+    class EntityDataFunction (*start)[64]; // 0x0000
+    void* last;                            // 0x0008
+    void* allocated_last;                  // 0x0010
+};                                         // Size: 0x0018
+
+class EntityDataFunction {
+public:
+    char pad_0000[8];               // 0x0000
+    class N000016EC* func_data;     // 0x0008
+    class Type_PRIMITIVE* ret_type; // 0x0010
+    char pad_0018[8];               // 0x0018
+    uint32_t hash;                  // 0x0020
+    char pad_0024[4];               // 0x0024
+};                                  // Size: 0x0028
+
+class N000016EC {
+public:
+    void* unk;  // 0x0000
+    void* func; // 0x0008
+};              // Size: 0x0010
+
+class N00000778 {
+public:
+    char pad_0000[8]; // 0x0000
+};                    // Size: 0x0008
+
+class N000007A5 {
+public:
+    class N0000083E (*start)[64]; // 0x0000
+    void* last;                   // 0x0008
+    void* allocated_last;         // 0x0010
+};                                // Size: 0x0018
+
+class N0000083E {
+public:
+    char pad_0000[32]; // 0x0000
+};                     // Size: 0x0020
+
+class ZSpatialEntity_RECLASS {
+public:
+    char pad_0000[8];                           // 0x0000
+    class ZEntityImpl_Inner N000008B9;          // 0x0008
+    char pad_0018[56];                          // 0x0018
+    Vector3 N00000910;                          // 0x0050
+    Vector3 N0000091C;                          // 0x005C
+    char pad_0068[4];                           // 0x0068
+    uint8_t N000008C5;                          // 0x006C
+    uint8_t N000008FB;                          // 0x006D
+    uint8_t N000008FE;                          // 0x006E
+    uint8_t N000008FC;                          // 0x006F
+    char pad_0070[328];                         // 0x0070
+    class TEntityRef_RECLASS (*bodyparts)[256]; // 0x01B8
+    void* N0000093A;                            // 0x01C0
+    void* N0000093B;                            // 0x01C8
+    char pad_01D0[912];                         // 0x01D0
+};                                              // Size: 0x0560
+
+class N000008C1 {
+public:
+    char pad_0000[8]; // 0x0000
+};                    // Size: 0x0008
+
+class N000009F7 {
 public:
     char pad_0000[8]; // 0x0000
 };                    // Size: 0x0008
